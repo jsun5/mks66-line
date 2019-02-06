424830617e41d5f4a35aa427108bb73ec6e7a852
jsun5 <jsun5@stuy.edu>
2019-02-06 15:37:15 -0500

    playing around with main, drawing random lines

--- draw.py
+++ draw.py
@@ -1,4 +1,7 @@
 from display import *
 
 def draw_line( x0, y0, x1, y1, screen, color ):
+    #octant 1
+    #if x0 > 0 and y0 > 0:
+     #   if y0>x0
     pass
--- main.py
+++ main.py
@@ -4,5 +4,8 @@ from draw import *
 screen = new_screen()
 color = [ 0, 255, 0 ]
 
+for i in range (200):
+    plot(screen,color,i,i)
+        
 display(screen)
 save_extension(screen, 'img.png')

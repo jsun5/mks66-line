c158f3f8aaa3bd2ba63aaf1ccf86fe3bbb941c7e
jsun5 <jsun5@stuy.edu>
2019-02-07 12:02:10 -0500

    added gitignore, started firt octant

--- draw.py
+++ draw.py
@@ -1,7 +1,27 @@
 from display import *
 
 def draw_line( x0, y0, x1, y1, screen, color ):
-    #octant 1
-    #if x0 > 0 and y0 > 0:
-     #   if y0>x0
-    pass
+    quad = -1
+    dy = (y1-y0)
+    dx = (x1-x0)
+    x = x0
+    y = y0
+    
+    if dy > 0 and dy > 0:
+        if dx > dy: quad = 1
+        else: quad = 2
+
+            
+    if quad is -1:
+        pass
+    if quad is 1:
+        A = dy
+        B = -dx
+        d = 2A + B
+        while x < x1:
+            plot(x,y)
+            if d>0:
+                y++
+                d+=2B
+            x++
+            d+=2A
